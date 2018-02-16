757aa005e5f0ff2f4e4e351987d4ae73e0b93895
raufzeynalov <rauf.zeynalow@gmail.com>
2018-02-16 19:27:43 +0100

    save state

--- models/hyperopt_model.py
+++ models/hyperopt_model.py
@@ -4,6 +4,7 @@ from time import time
 
 import numpy as np
 import pandas as pd
+import pickle
 from hyperopt import Trials, fmin, tpe, space_eval, STATUS_OK, STATUS_FAIL
 from sklearn.metrics import r2_score
 from sklearn.model_selection import KFold, cross_val_score
@@ -33,14 +34,16 @@ class HyperoptModel(AbstractRegressionModel):
         self.space = None
         self.best_ = None
         self._raw_features = None
-
+        self._output_prefix = output_prefix
         self.cv = cv
         self.n_jobs = n_jobs
         self.max_evals = max_evals
+        self.trials = None
         self.hyperopt_eval_num = 0
 
     def objective(self, params):
         """ objective function for the optimization process"""
+        pickle.dump(self.trials, open(self.output_prefix+".pkl", "wb"))
         start_time = time()
         self.pipeline.set_params(**params)
         shuffle = KFold(n_splits=self.cv, shuffle=True)
@@ -89,23 +92,39 @@ class HyperoptModel(AbstractRegressionModel):
     def fit(self):
         print("Performing parameters optimization...")
         with timing():
-            trials = Trials()
+            
+            self.trials = pickle.load(open(self.output_prefix+".pkl", "rb"))
+            print("Found saved Trials! Loading...")
+            count=0;
+            for index,x in enumerate(self.trials.losses()):
+                print(x)
+                if x is not None:
+                    count+=1
+             
+            print(count)
+            self.max_evals = count + self.max_evals        
+            self.hyperopt_eval_num = count  
+            for index in range(0, count):
+                print('[{0}/{1}]\tcv_eval_time={2:.2f} sec\tRMSE={3:.6f}\tR^2={4:.6f}'.format(
+            index+1, self.max_evals, self.trials.results[index].get('cv_eval_time'),self.trials.losses()[index], self.trials.results[index].get('r2')))
+                
+            print("Rerunning from {} trials to add another one.".format(count))  
             self.best_ = fmin(self.objective, self.space, algo=tpe.suggest,
-                              max_evals=self.max_evals, trials=trials, verbose=True)
+                              max_evals=self.max_evals, trials=self.trials, verbose=True)
 
             # save the hyperparameter at each iteration to a csv file
-            param_values = [x['misc']['vals'] for x in trials.trials]
+            param_values = [x['misc']['vals'] for x in self.trials.trials]
             param_values = [{key: value for key in x for value in x[key]} for x in param_values]
             param_values = [space_eval(self.space, x) for x in param_values]
 
             param_df = pd.DataFrame(param_values)
-            param_df['cv_eval_time'] = [r.get('cv_eval_time') for r in trials.results]
-            param_df['RMSE'] = trials.losses()
-            param_df['R^2'] = [r.get('r2') for r in trials.results]
+            param_df['cv_eval_time'] = [r.get('cv_eval_time') for r in self.trials.results]
+            param_df['RMSE'] = self.trials.losses()
+            param_df['R^2'] = [r.get('r2') for r in self.trials.results]
             param_df.index.name = 'Iteration'
             param_df.to_csv('./outputs/parameters/%s.csv' % self.output_prefix)
-        print()
-
+            pickle.dump(self.trials, open(self.output_prefix+".pkl", "wb"))
+        print()        
         best_parameters = space_eval(self.space, self.best_)
         self.pipeline.set_params(**best_parameters)
         self.model = self.pipeline.fit(self.X_train, self.y_train)
